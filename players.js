aeefc237f813ca4ccfb4587cec7b6e855d75dbf6
Ryan Webster <ryan.h.webster@gmail.com>
2022-02-10 12:46:44 -0800

    ryan

--- players.js
+++ players.js
@@ -1,9 +1,13 @@
 const Player = require('./playerClass');
 
-const zack = new Player('Zack', 'heads', 'over', 'other', 'pass', 'even', 'yes', 'eminem', 'over',
-    'no', 'yes', 'no', 'yes', '4', 'yes', 'yes', 'over', 'over',
+const ryan = new Player('Ryan', 'tails', 'under', 'Burrow', 'run', 'odd', 'yes', 
+    'snoop', 'over', 'yes', 'yes', 'yes', 'yes', '3', 'yes', 'yes', 'over', 'over',
+    'over', 'blue', 'no', 'rams');
+
+const zack = new Player('Zack', 'heads', 'over', 'other', 'pass', 'even', 'yes', 
+    'eminem', 'over', 'no', 'yes', 'no', 'yes', '4', 'yes', 'yes', 'over', 'over',
     'under', 'orange', 'yes', 'bengals');
 
-let players = [zack];
+let players = [ryan, zack];
 
 module.exports = players;
