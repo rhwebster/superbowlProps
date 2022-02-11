a73d7fea12680b81e524bbc81aae79ddcfb47825
Ryan Webster <ryan.h.webster@gmail.com>
2022-02-11 10:42:40 -0800

    reset results

--- bets.js
+++ bets.js
@@ -1,9 +1,9 @@
 const winningBets = {
-    coinToss: 'heads', anthem: null, firstPlayer: null, firstPlay: null,
+    coinToss: null, anthem: null, firstPlayer: null, firstPlay: null,
     firstHalfPts: null, nonQB: null, performer: null, halftimeSongs: null,
     snoopSmoke: null, thirdQtrScore: null, twoPt: null, dstTD: null, highQtr: null,
     bitcoinPrice: null, oneFifty: null, longestPlay: null, kuppChase: null,
-    staffordBurrow: null, gatorade: null, mvp: null, winner: 'rams'
+    staffordBurrow: null, gatorade: null, mvp: null, winner: null
 }
 
 module.exports = winningBets;
