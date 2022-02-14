9ac46f854087f336733cb8a9a71accdc29566a3e
Ryan Webster <ryan.h.webster@gmail.com>
2022-02-14 08:03:42 -0800

    final results

--- bets.js
+++ bets.js
@@ -2,7 +2,7 @@ const winningBets = {
     coinToss: 'heads', anthem: 'over', firstPlayer: 'other', firstPlay: 'run',
     firstHalfPts: 'odd', nonQB: 'yes', performer: 'dre', halftimeSongs: 'over',
     snoopSmoke: 'no', thirdQtrScore: 'yes', twoPt: 'no', dstTD: 'no', highQtr: '3',
-    bitcoinPrice: 'no', oneFifty: 'under', longestPlay: 'over', kuppChase: 'under',
+    bitcoinPrice: 'no', oneFifty: 'no', longestPlay: 'over', kuppChase: 'under',
     staffordBurrow: 'over', gatorade: 'blue', mvp: 'no', winner: 'bengals'
 }
 
---
+++ payout.js
@@ -0,0 +1,26 @@
+const bets = require('./bets');
+const players = require('./players');
+
+function sbProps(obj, arr) {
+    for (const key in obj) {
+        payout(arr, obj, key)
+    }
+    
+    arr.sort((a,b) => b.winnings - a.winnings);
+    return arr.forEach(player => console.log(`${player.name} : ${player.winnings}`));
+}
+
+function payout(arr, obj, key) {
+    let betWinners = [];
+
+    betWinners = arr.filter(player => player[key] === obj[key]);
+
+    let betPayout = arr.length / betWinners.length;
+
+    if (key === 'winner') betPayout *= 5;
+    console.log(key, betPayout);
+
+    betWinners.forEach(winner => winner['winnings'] += betPayout);
+}
+
+sbProps(bets, players);
