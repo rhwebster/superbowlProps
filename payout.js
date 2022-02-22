a7e4a48f25f86b83c3609f3194ffb20550066d36
Ryan Webster <ryan.h.webster@gmail.com>
2022-02-22 14:12:34 -0800

    cleanup

--- payout.js
+++ payout.js
@@ -1,25 +1,19 @@
 const bets = require('./bets');
 const players = require('./players');
 
-const sbProps = (obj, arr) => {
+const propsPayout = (obj, arr) => {
     for (const key in obj) {
-        payout(arr, obj, key)
+        let betWinners = arr.filter(player => player[key] === obj[key]);
+
+        let betPayout = arr.length / betWinners.length;
+
+        if (key === 'winner') betPayout *= 5;
+
+        betWinners.forEach(winner => winner['winnings'] += betPayout);
     }
     
     return arr.sort((a, b) => b.winnings - a.winnings)
         .forEach(player => console.log(`${player.name} : ${player.winnings}`));
 }
 
-const payout = (arr, obj, key) => {
-    let betWinners = [];
-
-    betWinners = arr.filter(player => player[key] === obj[key]);
-
-    let betPayout = arr.length / betWinners.length;
-
-    if (key === 'winner') betPayout *= 5;
-
-    betWinners.forEach(winner => winner['winnings'] += betPayout);
-}
-
-sbProps(bets, players);
+propsPayout(bets, players);
